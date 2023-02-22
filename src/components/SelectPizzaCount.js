43195c047b1ac87eb407118cda39d3285f669823
1oky <1oky.dimasik@gmail.com>
2023-02-22 19:31:45 +0300

    fix bugs

--- src/components/SelectPizzaCount.js
+++ src/components/SelectPizzaCount.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-const SelectPizzaCount = ({id, setActive, activeCount}) => {
+const SelectPizzaCount = ({ id, setActive, activeCount }) => {
   const [isActive, setIsActive] = React.useState(false);
-  
+
   const handleClick = () => {
-    setIsActive(current => !current)
-    setActive(id)
+    setIsActive(current => !current);
+    setActive(id);
   }
-    return (
-        <div className='pizza-counter'>
-          <button onClick={handleClick}>
-            <img style={{
-              backgroundColor: id <= activeCount ? '#ff6900' : 'white',
-            }} className='pizza-icon' src='./images/pizza-icon.svg' alt='pizza-icon'>
-            </img>
-          </button>
-        </div>
-    );
+  return (
+    <div className='pizza-counter'>
+      <button onClick={handleClick}>
+        <img style={{
+          backgroundColor: id <= activeCount ? '#ff6900' : 'white',
+        }} className='pizza-icon' src='./images/pizza-icon.svg' alt='pizza-icon'>
+        </img>
+      </button>
+    </div>
+  );
 };
 
 export default SelectPizzaCount;
