c2dec21b6a545b206b7987eea8699af3552b605f
1oky <1oky.dimasik@gmail.com>
2023-02-22 19:30:58 +0300

    add pizza card with a random selection func

--- src/components/RandomizerCard.js
+++ src/components/RandomizerCard.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
-const RandomizerCard = ({countOfCards}) => {
-  console.log(countOfCards)
-    return (
-        <div className='pizza-card'>
-          <img src='./images/pizza.webp'></img>
-          <h4>Ветчина сыр</h4>
-          <span>666 руб.</span>
-        </div>
-    );
+const RandomizerCard = () => {
+  const [randomPizza, setRandomPizza] = React.useState({name: 'Ветчина и грибы', imageUrl: './images/DodoPizzaImages/vetchinagriby.webp', price: '389'})
+  
+  function randomPizzaIndex () {
+    return Math.floor(Math.random() * (24 - 1 + 1) + 1);
+    console.log(randomPizzaIndex())
+  };
+  console.log(randomPizzaIndex())
+
+  const handleClick = () => {
+    async function fetchData() {
+      await axios
+      .get("https://63ebe7aa31ef61473b1b2e23.mockapi.io/Pizza")
+      .then((res) => {
+        setRandomPizza(res.data[randomPizzaIndex()]);
+      })
+    }
+    fetchData()
+  }
+  
+  React.useEffect(() => {
+    handleClick()
+  }, [])
+
+  return (
+    <div onClick={handleClick} className='pizza-card'>
+      <img src={randomPizza.imageUrl} alt='Пицца'></img>
+      <h4>{randomPizza.name}</h4>
+      <span>от {randomPizza.price} руб.</span>
+    </div>
+  );
 };
 
 export default RandomizerCard;
